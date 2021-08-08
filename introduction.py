1493c36fcfe77f3604f4552987df0c519f399077
Johnny <contact@y0ham.com>
2021-08-08 08:42:25 +0300

    update

--- introduction.py
+++ introduction.py
@@ -10,9 +10,9 @@ network.add(layers.Dense(10, activation="softmax"))
 network.compile(optimizer="rmsprop", loss="categorical_crossentropy", metrics=["accuracy"])
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
 train_images = train_images.reshape((60000, 28 * 28))
 train_images = train_images.astype("float32") / 255
-
 test_images = test_images.reshape((10000, 28 * 28))
 test_images = test_images.astype('float32') / 255
 
